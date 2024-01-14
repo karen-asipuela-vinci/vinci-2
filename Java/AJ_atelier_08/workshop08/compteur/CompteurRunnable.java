31ebdbe4d6cfbd5fa2d041aae46c490d41c0b6c3
karen.asipuela@student.vinci.be <karen.asipuela@student.vinci.be>
2024-01-14 19:01:53 +0100

    atelier 8 : fin 3.2

--- Java/AJ_atelier_08/workshop08/compteur/CompteurRunnable.java
+++ Java/AJ_atelier_08/workshop08/compteur/CompteurRunnable.java
@@ -1,11 +1,15 @@
 package compteur;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class CompteurRunnable implements Runnable {
 
     private String nom;
     private int max;
 
     //TODO: ajouter un attribut de classe qui retient l'ordre d'arrivée.
+    // AtomicInteger : An int value that may be updated atomically.
+    private static AtomicInteger ordreArrivee = new AtomicInteger();
 
     @Override
     public void run() {
@@ -13,6 +17,18 @@ public class CompteurRunnable implements Runnable {
         //         A chaque incrémentation, afficher le nom du compteur et son compte actuel.
         //      2. Quand le compte est terminé, afficher que le compteur a finit de compter
         //         et indiquer son ordre d'arrivée.
+        for (int i = 0; i < max; i++) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(nom + " : " + i );
+        }
+
+        //finComptage
+        System.out.println(nom + " a fini de compter. Sa position : " + ordreArrivee.incrementAndGet());
     }
 
     public CompteurRunnable(String nom, int max) {
