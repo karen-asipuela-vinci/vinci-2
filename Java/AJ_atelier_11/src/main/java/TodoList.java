936bfa338234540ef712dd8ecf78f0cba2cc5442
karen.asipuela@student.vinci.be <karen.asipuela@student.vinci.be>
2024-01-19 02:14:53 +0100

    premier test : addTask : d'abord code puis création de classe et de tout ce qui est rouge

---
+++ Java/AJ_atelier_11/src/main/java/TodoList.java
@@ -0,0 +1,9 @@
+public class TodoList {
+    public boolean add(String task1) {
+        return false;
+    }
+
+    public boolean containsTask(String task1) {
+        return false;
+    }
+}
---
+++ Java/AJ_atelier_11/src/test/java/TodoListTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static java.util.random.RandomGeneratorFactory.all;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TodoListTest {
+    @Test
+    void addTast() {
+        TodoList todoList = new TodoList();
+
+        assertAll(
+            () -> assertTrue(todoList.add("task1")),
+            () -> assertTrue(todoList.containsTask("task1"))
+        );
+    }
+}
