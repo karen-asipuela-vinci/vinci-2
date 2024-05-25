9f014b6874cb15a7a050dbddde6982b490b3cdab
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2024-05-25 20:12:24 +0200

    sélection de mode

--- intro-mobile/ex2/lib/home_screen.dart
+++ intro-mobile/ex2/lib/home_screen.dart
@@ -1,16 +1,54 @@
+import 'package:ex2/property.dart';
 import 'package:ex2/property_widget.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // rajout de la liste des propriétés
+  var showToBuy = false;
+  void _toggleShowToBuy() => setState(() => showToBuy = !showToBuy);
+
+  @override
   Widget build(BuildContext context) {
+    // modif pour méthode de tri
+    final diplayedProperties = [
+      for (final property in properties)
+        if (showToBuy && property.isToRent || !showToBuy && !property.isToRent)
+          property
+    ];
+
+    // on rajoute un bouton pour changer le tri
     return Scaffold(
       appBar: AppBar(
         title: const Text('Exo 2 : liste de propriétés'),
+        // rajout du bouton Switch pour changer le tri dans actions
+        actions: [
+          const Text('Buy'),
+          Switch(
+            value: showToBuy,
+            onChanged: (value) => _toggleShowToBuy(),
+          ),
+          const Text('Rent'),
+        ],
       ),
-      body: PropertyWidget(),
+      body: Container(
+        padding: const EdgeInsets.all(8),
+        child: Center(
+          child: SizedBox(
+            width: 512.0,
+            child: ListView.builder(
+              itemCount: diplayedProperties.length,
+              itemBuilder: (context, index) => PropertyWidget(property: diplayedProperties[index]),
+            )
+          )
+        )
+      ),
     );
   }
 }
--- intro-mobile/ex2/lib/property_widget.dart
+++ intro-mobile/ex2/lib/property_widget.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class PropertyWidget extends StatelessWidget {
+  final Property property;
+
+  // rajout du constructeur
+  const PropertyWidget({Key? key, required this.property}) : super(key: key);
+
   // afficher les informations des propriétés
   // utiliser ListTile
   @override
@@ -11,15 +16,16 @@ class PropertyWidget extends StatelessWidget {
     // ici, espace entre milliers et virgule pour les décimales
     // pour l'utiliser : intl dans pubspec.yaml
     final formatCurrency = NumberFormat.simpleCurrency(locale: 'fr_FR');
-    return ListView(
-      children: properties.map((property) {
-        return ListTile(
-          leading: Icon(property.isAHouse ? Icons.home : Icons.apartment),
-          title: Text('${property.isToRent ? 'To rent' : 'To sell'} : ${property.isAHouse ? 'house' : 'apartment'} of ${property.squareFootage} m2'),
-          subtitle: Text('Price: ${formatCurrency.format(property.price)}€'),
-          trailing: Text('Bedrooms: ${property.numberOfBedrooms}'),
-        );
-      }).toList(),
+    return ListTile(
+      title: Text('${property.isToRent ? 'To rent' : 'To buy'} : ${property.isAHouse ? 'house' : 'flat'} of ${property.squareFootage} m2'),
+      subtitle: Text('Price: ${formatCurrency.format(property.price)}'),
+      trailing: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Surface: ${property.squareFootage} m²'),
+          Text('Bedrooms: ${property.numberOfBedrooms}')
+        ],
+      ),
     );
   }
 }
