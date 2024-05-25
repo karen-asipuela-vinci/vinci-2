adbb4a8aa0839f2c233673d13a7fd8e14cfa034c
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2024-05-25 19:24:35 +0200

    filtrage liste

--- intro-mobile/tuto2/lib/home_screen.dart
+++ intro-mobile/tuto2/lib/home_screen.dart
@@ -5,23 +5,43 @@ import 'package:flutter/material.dart';
 import 'contact.dart';
 import 'contact_row.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var showFavorites = false;
+  void _toggleFavorites() => setState(() => showFavorites = !showFavorites);
+
   @override
   Widget build(BuildContext context) {
+    //on modif pour afficher les contacts favoris
+    final displayedContacts = [
+      for (var contact in defaultContacts)
+        if (!showFavorites || contact.isFavorite) contact
+    ];
+
+    //on ajoute un bouton pour afficher les contacts favoris
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Contact list"),
+        actions: [
+          IconButton(
+            icon: Icon(showFavorites ? Icons.star : Icons.star_border),
+            onPressed: _toggleFavorites,
+          ),
+        ],
       ),
-      body: Padding(
+      body: Container(
         padding: const EdgeInsets.all(16.0),
         child: Center(
           child: SizedBox(
-            width: 512,
+            width: 512.0,
             child: ListView.builder(
-              itemCount: defaultContacts.length,
+              itemCount: displayedContacts.length,
               itemBuilder: (context, index) =>
-                  ContactRow(contact: defaultContacts[index]),
+                  ContactRow(contact: displayedContacts[index]),
             ),
           ),
         ),
