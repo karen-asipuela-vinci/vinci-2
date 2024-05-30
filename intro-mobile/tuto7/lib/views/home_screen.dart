422be1b5661467e0eff085e5707ed9bed8b7ec3b
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2024-05-30 07:46:01 +0200

    appel à un dialog

--- intro-mobile/tuto7/lib/views/home_screen.dart
+++ intro-mobile/tuto7/lib/views/home_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sms/flutter_sms.dart';
+import 'location_dialog.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -51,6 +52,14 @@ class HomeScreen extends StatelessWidget {
                 )
               else
                 const Text("Your platform doesn't allow you to send SMS…"),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () => showDialog(
+                  context: context,
+                  builder: (context) => const LocationDialog(),
+                ),
+                child: const Text("Retrieve location"),
+              ),
             ],
           ),
         ),
