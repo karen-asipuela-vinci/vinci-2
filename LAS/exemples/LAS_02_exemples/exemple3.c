87811631aa24a8d11c7c5ebf6c783d3b5296d878
kaeli <karen.asipuela@student.vinci.be>
2024-06-01 20:10:55 +0200

    comms 3

--- LAS/exemples/LAS_02_exemples/exemple3.c
+++ LAS/exemples/LAS_02_exemples/exemple3.c
@@ -1,4 +1,4 @@
-/// version utilisant les versions "safe" des syscalls 
+/// version utilisant les versions "safe" des syscalls
 /// fournies dans le module utils (sfork, swaitpid, sexecl)
 
 #include <stdbool.h>
@@ -13,23 +13,32 @@
 int main(int argc, char **argv)
 {
   /* Création d'un processus fils */
+  // sfork() est une version sécurisée de fork() qui termine le programme en cas d'échec
   int childId = sfork();
 
-  if (childId != 0) 
+  if (childId != 0)
   {
     /* Dans le processus parent */
-    printf("Processus parent (pid=%i) en attente de la fin de son enfant (pid fils=%i).\n",getpid(),childId);
+    // Affiche l'ID du processus parent et de l'enfant
+    printf("Processus parent (pid=%i) en attente de la fin de son enfant (pid fils=%i).\n", getpid(), childId);
 
     /* Attente de la terminaison du processus fils */
+    // swaitpid() est une version sécurisée de waitpid() qui termine le programme en cas d'échec
+    // waitpid() attend que le processus spécifié par childId se termine
     int status;
     swaitpid(childId, &status, 0);
-    
-    printf("Processus parent se termine après son enfant (pid fils=%i).\n",childId);
+
+    // Affiche un message indiquant que le processus parent se termine après son enfant
+    printf("Processus parent se termine après son enfant (pid fils=%i).\n", childId);
   }
   else
   {
     /* Dans le processus fils */
+    // Affiche l'ID du processus fils et de son parent
     printf("Je suis le fils '%s': pid=%i - ppid=%i\n", argv[0], getpid(), getppid());
+
+    // sexecl() est une version sécurisée de execl() qui termine le programme en cas d'échec
+    // execl() remplace le processus fils actuel par un nouveau processus
     sexecl("./myScript.sh", "myScript.sh", NULL);
   }
 }
