7759e1b3bd6f3f90b7f7022acc5d01b38280b32f
kaeli <karen.asipuela@student.vinci.be>
2024-06-01 20:05:45 +0200

    commentaires 2c

--- LAS/exemples/LAS_02_exemples/exemple2.c
+++ LAS/exemples/LAS_02_exemples/exemple2.c
@@ -9,29 +9,31 @@
 
 #include "utils_v1.h"
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
-  /* Création d'un processus fils */
-  int childId = fork();
-  checkCond(childId == -1, "Fork failed");
+  // Création d'un processus fils
+  int childId = fork();                    // fork() crée un nouveau processus en dupliquant le processus existant
+  checkCond(childId == -1, "Fork failed"); // Vérifie si la création du processus a échoué
 
-  if (childId != 0)   // childId = pid de l’enfant
+  if (childId != 0) // Si childId est différent de 0, nous sommes dans le processus parent
   {
-    /* Dans le processus parent */
-    printf("Processus parent (pid=%i) en attente de la fin de son enfant (pid fils=%i).\n",getpid(),childId);
+    // Affiche l'ID du processus parent et de l'enfant
+    printf("Processus parent (pid=%i) en attente de la fin de son enfant (pid fils=%i).\n", getpid(), childId);
 
-    /* Attente de la terminaison du processus fils */
-    int status;   // statut de l’enfant terminé
-    int waitId = waitpid(childId, &status, 0);   // waitId = pid renvoyé par le wait (=childId)
-    checkCond(waitId == -1, "Wait error");
-    
-    printf("Processus parent se termine après son enfant (pid fils=%i).\n",childId);
-  } 
-  else 
-  { 
-    /* Dans le processus fils */
-    printf("Je suis le fils '%s': pid=%i - ppid=%i\n",argv[0],getpid(),getppid());
-    execl("./myScript.sh","myScript.sh", NULL);
-    checkCond(true, "Exec failed");
+    // Attente de la terminaison du processus fils
+    int status;                                // statut de l’enfant terminé
+    int waitId = waitpid(childId, &status, 0); // waitpid() attend que le processus spécifié par childId se termine
+    checkCond(waitId == -1, "Wait error");     // Vérifie si l'attente a échoué
+
+    // Affiche un message indiquant que le processus parent se termine après son enfant
+    printf("Processus parent se termine après son enfant (pid fils=%i).\n", childId);
+  }
+  else
+  {
+    // Si childId est égal à 0, nous sommes dans le processus fils
+    printf("Je suis le fils '%s': pid=%i - ppid=%i\n", argv[0], getpid(), getppid()); // Affiche l'ID du processus fils et de son parent
+
+    execl("./myScript.sh", "myScript.sh", NULL); // execl() remplace le processus fils actuel par un nouveau processus
+    checkCond(true, "Exec failed");              // Vérifie si la création du nouveau processus a échoué
   }
 }
