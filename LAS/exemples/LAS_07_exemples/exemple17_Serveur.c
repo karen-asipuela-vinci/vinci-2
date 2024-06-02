50514f6b596c2971b3d914204891bcc7f869f523
kaeli <karen.asipuela@student.vinci.be>
2024-06-02 12:00:49 +0200

    coms 17

--- LAS/exemples/LAS_07_exemples/exemple17_Serveur.c
+++ LAS/exemples/LAS_07_exemples/exemple17_Serveur.c
@@ -13,68 +13,68 @@
 #define PORT 9090
 
 /**
- * Basic sample that shows the different steps to have a listening server.
+ * Exemple basique qui montre les différentes étapes pour avoir un serveur en écoute.
  *
- * In this sample, syscalls are not checked to concentrate on the different steps
- * but you must check the syscalls for the exercices !
+ * Dans cet exemple, les appels système ne sont pas vérifiés pour se concentrer sur les différentes étapes
+ * ! mais vous devez vérifier les appels système pour les exercices !
  */
 
-// Unsafe version as shown in videos
+// ! Version non sécurisée comme montré dans les vidéos
 int main_unsafe_version(int argc, char *arg[])
 {
   struct sockaddr_in addr;
-  // socket creation
+  // création du socket
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-  // setsockopt -> to avoid Address Already in Use
+  // setsockopt -> pour éviter l'erreur "Adresse déjà utilisée"
   int option = 1;
   setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(int));
 
-  // prepare sockaddr to bind
+  // préparation de sockaddr pour le bind
   memset(&addr, 0, sizeof(addr));
   addr.sin_family = AF_INET;
-  // listen port 9090 !!!
+  // écoute sur le port 9090 !!!
   addr.sin_port = htons(PORT);
-  // listen on all server interfaces
+  // écoute sur toutes les interfaces du serveur
   addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
   bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
-  // simultaneous client max -> 5
+  // nombre maximum de clients simultanés -> 5
   listen(sockfd, 5);
   printf("Le serveur tourne sur le port %d\n", PORT);
-  // accept client connection
+  // acceptation de la connexion client
   int newsockfd = accept(sockfd, NULL, NULL);
-  // read message from client
+  // lecture du message du client
   int val;
   read(newsockfd, &val, sizeof(int));
   printf("Valeur reçue : %i\n", val);
-  // close connection client
+  // fermeture de la connexion client
   close(newsockfd);
-  // close listen socket
+  // fermeture du socket d'écoute
   close(sockfd);
 }
 
-// Safe version with safe functions
+// ! Version sécurisée avec des fonctions sécurisées
 int main(int argc, char *arg[])
 {
   struct sockaddr_in addr;
 
-  // socket creation
+  // création du socket
   int sockfd = ssocket();
   sbind(PORT, sockfd);
   slisten(sockfd, BACKLOG);
   printf("Le serveur tourne sur le port %d\n", PORT);
-  
-  // accept client connection
+
+  // acceptation de la connexion client
   int newsockfd = saccept(sockfd);
-  
-  // read message from client
+
+  // lecture du message du client
   int val;
   sread(newsockfd, &val, sizeof(int));
   printf("Valeur reçue : %i\n", val);
-  
-  // close connection client
+
+  // fermeture de la connexion client
   sclose(newsockfd);
-  // close listen socket
+  // fermeture du socket d'écoute
   sclose(sockfd);
 }
