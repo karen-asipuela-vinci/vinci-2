600efcfc0885b57db237d0178dbb57c0fa565b90
kaeli <karen.asipuela@student.vinci.be>
2024-06-02 12:12:10 +0200

    coms 19 avec poll

--- LAS/exemples/LAS_08_exemples/exemple19_avec_poll/client.c
+++ LAS/exemples/LAS_08_exemples/exemple19_avec_poll/client.c
@@ -16,37 +16,49 @@
 // POST: on success connects a client socket to ServerIP:port
 //       return socket file descriptor
 //       on failure, displays error cause and quits the program
+// Cette fonction initialise un socket client et le connecte à un serveur spécifié par son IP et son port.
+// En cas de succès, elle renvoie le descripteur de fichier du socket.
+// En cas d'échec, elle affiche la cause de l'erreur et quitte le programme.
 int initSocketClient(char ServerIP[16], int Serverport)
 {
-	int sockfd = ssocket();
+	int sockfd = ssocket(); // crée un nouveau socket
+	// socket = point de terminaison d'une communication bidirectionnelle entre 2 programmes sur un réseau
+	// communication entre 2 sockets
 	sconnect(ServerIP, Serverport, sockfd);
 	return sockfd;
 }
 
 int main(int argc, char **argv)
 {
+	// vérifie que le nombre d'arguments est correct
 	if (argc != 3)
 	{
 		printf("%s\n", "Usage argv[0] ServerIP ServerPort");
 		exit(1);
 	}
 
-	// initClient before read keyboard ... not a good idea
+	// initialise le socket client et le connecte au serveur
+	// serveur = atoi(argv[2])?
 	int sockfd = initSocketClient(argv[1], atoi(argv[2]));
 
 	printf("Bienvenue dans le programe vache - Entrez votre pseudo\n");
 
-	char msg[MESSAGE_SIZE];
+	char msg[MESSAGE_SIZE]; // crée un buffer pour stocker les messages
+
+	// lit le message de l'utilisateur à partir de l'entrée standard (clavier)
 	sread(0, msg, 256);
 
+	// envoie le message au serveur
 	swrite(sockfd, msg, sizeof(msg));
 
-	/* wait server response */
+	// attend une réponse du serveur
 	sread(sockfd, msg, sizeof(msg));
 
+	// affiche la réponse du serveur
 	printf("Réponse du serveur : \n");
 	printf("%s\n", msg);
 
+	// !fermer le socket client
 	sclose(sockfd);
 
 	return 0;
