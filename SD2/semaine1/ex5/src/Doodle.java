601a8e2dbe42f2166c9fd5fd7e83b5bbe8cc086c
karen.asipuela@student.vinci.be <karen.asipuela@student.vinci.be>
2024-06-09 13:58:55 +0200

    exo 5

--- SD2/semaine1/ex5/src/Doodle.java
+++ SD2/semaine1/ex5/src/Doodle.java
@@ -1,35 +1,45 @@
+import java.util.*;
+
 public class Doodle {
-	private PlageHoraire[] plages;
-	// a compl�ter
+    private List<PlageHoraire> plages;
+    private Map<String, boolean[]> disponibilites;
+    private int[] compteurParticipants;
 
-	public Doodle(PlageHoraire... plages) {
-		this.plages = plages;
-		// a compl�ter
-	}
+    public Doodle(PlageHoraire... plages) {
+        this.plages = new ArrayList<>(List.of(plages));
+        this.disponibilites = new HashMap<>();
+        this.compteurParticipants = new int[plages.length];
+    }
 
-	// ajoute les disponibilit�s d'un participant sous forme d'un tableau de booleen.
-	// les indices du tableau correspondent aux indices du tableau plages
-	// true � l'indice i veut dire que le participant est disponible pour la plage � l'indice i du tableau plages
-	// false � l'indice i veut dire que le participant n'est pas disponible pour la plage � l'indice i du tableau plages
-	public void ajouterDisponibilites(String participant,
-			boolean[] disponibilites) {
-		if (disponibilites.length != plages.length)
-			throw new IllegalArgumentException();
-		//a compl�ter
-	}
-	
-	// Hypoth�se: la PlageHoraire plage en param�tre fait bien partie du tableau plages
-	// renvoie vrai si le participant est disponible pour cette plage horaire
-	// renvoie faux si le participant n'est pas disponible ou s'il n'a pas rempli le
-	// sondage doodle
-	public boolean estDisponible(String participant, PlageHoraire plage) {
-		return false;
-	}
+    public void ajouterDisponibilites(String participant, boolean[] disponibilites) {
+        if (disponibilites.length != plages.size())
+            throw new IllegalArgumentException();
+        this.disponibilites.put(participant, disponibilites);
+        for (int i = 0; i < disponibilites.length; i++) {
+            if (disponibilites[i]) {
+                compteurParticipants[i]++;
+            }
+        }
+    }
 
-	// renvoie une des plages horaires qui a le maximum de participants pr�vus
-	// cette m�thode est appel�e apr�s que les participants aient rempli leurs disponibilit�s
-	public PlageHoraire trouverPlageQuiConvientLeMieux() {
-		return null;
-	}
+    public boolean estDisponible(String participant, PlageHoraire plage) {
+        int index = plages.indexOf(plage);
+        if (index == -1 || !disponibilites.containsKey(participant)) {
+            return false;
+        }
+        return disponibilites.get(participant)[index];
+    }
 
+    public PlageHoraire trouverPlageQuiConvientLeMieux() {
+        int maxParticipants = -1;
+        PlageHoraire bestPlage = null;
+        for (int i = 0; i < plages.size(); i++) {
+            if (compteurParticipants[i] > maxParticipants) {
+                maxParticipants = compteurParticipants[i];
+                bestPlage = plages.get(i);
+            }
+        }
+        bestPlage.setNbParticipantPresent(maxParticipants);
+        return bestPlage;
+    }
 }
