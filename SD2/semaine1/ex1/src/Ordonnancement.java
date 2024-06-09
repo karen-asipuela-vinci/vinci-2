4dc358f63bc1e6564ac2b07d14629a366902297b
karen.asipuela@student.vinci.be <karen.asipuela@student.vinci.be>
2024-06-09 12:56:25 +0200

    exo1

---
+++ SD2/semaine1/ex1/src/Ordonnancement.java
@@ -0,0 +1,17 @@
+public class Ordonnancement {
+	public static final int NIVEAU_PRIORITE_MAX=5;
+
+	public Ordonnancement(){
+
+	}
+	public void ajouterTache (String descriptif, int priorite){
+
+	}
+	
+	//renvoie la tache prioritaire
+	//renvoie null si plus de tache presente
+	public Tache attribuerTache(){
+		
+		return null;
+	}
+}
---
+++ SD2/semaine1/ex2/src/ControleDAcces.java
@@ -0,0 +1,25 @@
+public class ControleDAcces {
+	
+	public ControleDAcces(){
+		
+	}
+	
+	// associe le badge � un employ�
+	public void donnerBadge (Badge b, Employe e){
+	}
+	
+	// met � jour les employ�s pr�sents dans le batiment
+	public void entrerBatiment (Badge b){
+	}
+
+	// met � jour les employ�s pr�sents dans le batiment
+	public void sortirBatiment (Badge b){
+
+	}
+	
+	// renvoie vrai si l'employ� est dans le batiment, faux sinon
+	public boolean estDansBatiment (Employe e){
+		return false;
+	}
+
+}
