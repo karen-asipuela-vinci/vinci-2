f6d397a2035fea1760ea485735e861ad65c0f6aa
karen.asipuela@student.vinci.be <karen.asipuela@student.vinci.be>
2024-06-09 12:58:30 +0200

    exo 1

--- SD2/semaine1/ex1/src/Ordonnancement.java
+++ SD2/semaine1/ex1/src/Ordonnancement.java
@@ -1,17 +1,29 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Ordonnancement {
 	public static final int NIVEAU_PRIORITE_MAX=5;
+	private PriorityQueue<Tache> taches;
 
 	public Ordonnancement(){
-
+				taches = new PriorityQueue<Tache>(
+				Comparator.comparing(Tache::getPriorite).reversed()
+						.thenComparing(Tache::getDate)
+		);
 	}
 	public void ajouterTache (String descriptif, int priorite){
-
+		if(priorite<1 || priorite>NIVEAU_PRIORITE_MAX){
+			throw new IllegalArgumentException("Priorité invalide");
+		}
+		Tache t = new Tache(descriptif, priorite);
+		taches.add(t);
 	}
 	
 	//renvoie la tache prioritaire
 	//renvoie null si plus de tache presente
 	public Tache attribuerTache(){
-		
-		return null;
+		// pas besoin de tester si taches est vide
+		// car poll renvoie null si vide
+		return taches.poll();
 	}
 }
