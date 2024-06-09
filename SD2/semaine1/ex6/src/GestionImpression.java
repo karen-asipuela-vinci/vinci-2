532c95868c2041a5eea3d280493fc7412278885e
karen.asipuela@student.vinci.be <karen.asipuela@student.vinci.be>
2024-06-09 14:06:09 +0200

    exo 6

--- SD2/semaine1/ex6/src/GestionImpression.java
+++ SD2/semaine1/ex6/src/GestionImpression.java
@@ -1,14 +1,26 @@
+import java.util.*;
+
 public class GestionImpression {
-	
+    private Queue<Impression> queue;
+    private Map<String, Impression> lastImpressionByUser;
 
-	
-	public void ajouterImpression(Impression impr){
+    public GestionImpression() {
+        this.queue = new LinkedList<>();
+        this.lastImpressionByUser = new HashMap<>();
+    }
 
-	}
-	
-	public Impression selectionnerImpression(){
+    public void ajouterImpression(Impression impr) {
+        queue.add(impr);
+        lastImpressionByUser.put(impr.getIdUtilisateur(), impr);
+    }
 
-	}
-
-
+    public Impression selectionnerImpression() {
+        Impression nextImpression;
+        while ((nextImpression = queue.poll()) != null) {
+            if (nextImpression == lastImpressionByUser.get(nextImpression.getIdUtilisateur())) {
+                return nextImpression;
+            }
+        }
+        return null;
+    }
 }
