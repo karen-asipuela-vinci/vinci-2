9d521caf24c678af7c4b695f908ff06678fd537e
karen.asipuela@student.vinci.be <karen.asipuela@student.vinci.be>
2024-06-09 13:30:06 +0200

    exo3

--- SD2/semaine1/ex2/src/ControleDAcces.java
+++ SD2/semaine1/ex2/src/ControleDAcces.java
@@ -1,25 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class ControleDAcces {
+	private HashMap<Badge, Employe> badges;
+	private HashSet<Employe> employesPresents;
 	
 	public ControleDAcces(){
-		
+		badges = new HashMap<Badge, Employe>();
+		employesPresents = new HashSet<>();
 	}
 	
 	// associe le badge � un employ�
 	public void donnerBadge (Badge b, Employe e){
+		badges.put(b, e);
 	}
 	
 	// met � jour les employ�s pr�sents dans le batiment
 	public void entrerBatiment (Badge b){
+		Employe e = badges.get(b);
+		if(e != null){
+			employesPresents.add(e);
+		}
 	}
 
 	// met � jour les employ�s pr�sents dans le batiment
 	public void sortirBatiment (Badge b){
-
+		Employe e = badges.get(b);
+		if(e != null){
+			employesPresents.remove(e);
+		}
 	}
 	
 	// renvoie vrai si l'employ� est dans le batiment, faux sinon
 	public boolean estDansBatiment (Employe e){
-		return false;
+		return employesPresents.contains(e);
 	}
-
 }
--- SD2/semaine1/ex3/src/LocationPatins.java
+++ SD2/semaine1/ex3/src/LocationPatins.java
@@ -1,31 +1,41 @@
 import java.time.LocalTime;
+import java.util.*;
 
 import static java.time.temporal.ChronoUnit.MILLIS;
 
 public class LocationPatins {
-	
-	public LocationPatins(int[] casiers) {
-	
-	}
+    private HashMap<Integer, Integer> pointures;
+    private HashMap<Integer, List<Integer>> casiersDisponibles;
+    private HashMap<Integer, LocalTime> casiersOccupes;
 
-	// date1 < date2
-	private static double prix(LocalTime date1, LocalTime date2) {
-		// 1 euro par milliseconde (c'est assez cher en effet)
-		return MILLIS.between(date1, date2) ;
-	}
+    public LocationPatins(int[] casiers) {
+        pointures = new HashMap<>();
+        casiersDisponibles = new HashMap<>();
+        casiersOccupes = new HashMap<>();
+        for (int i = 0; i < casiers.length; i++) {
+            pointures.put(i, casiers[i]);
+            casiersDisponibles.computeIfAbsent(casiers[i], k -> new ArrayList<>()).add(i);
+        }
+    }
 
-	public int attribuerCasierAvecPatins(int pointure) {
-		if (pointure < 33 || pointure > 48)
-			throw new IllegalArgumentException();
-		LocalTime l = LocalTime.now();
-		
-		
-		//a compl�ter
+    private static double prix(LocalTime date1, LocalTime date2) {
+        return MILLIS.between(date1, date2);
+    }
 
-	}
+    public int attribuerCasierAvecPatins(int pointure) {
+        if (pointure < 33 || pointure > 48 || !casiersDisponibles.containsKey(pointure) || casiersDisponibles.get(pointure).isEmpty())
+            return -1;
+        int casier = casiersDisponibles.get(pointure).remove(0);
+        casiersOccupes.put(casier, LocalTime.now());
+        return casier;
+    }
 
-	public double libererCasier(int numeroCasier) {
-		//a completer
-	}
-
+    public double libererCasier(int numeroCasier) {
+        if (!pointures.containsKey(numeroCasier) || !casiersOccupes.containsKey(numeroCasier))
+            return -1;
+        double prix = prix(casiersOccupes.get(numeroCasier), LocalTime.now());
+        casiersOccupes.remove(numeroCasier);
+        casiersDisponibles.get(pointures.get(numeroCasier)).add(numeroCasier);
+        return prix;
+    }
 }
