980210713980541a3fe87048cf096c42431f1c9b
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2024-06-17 21:29:22 -0500

    modif gitignore

---
+++ intro-mobile/tuto1/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:intro_mobile/product_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Column(
+            children: [
+              ProductWidget(
+                name: "iPhone 15 Pro Max",
+                price: 1479,
+                description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                    "Aliquam et risus vel ipsum faucibus ultrices. "
+                    "Fusce nec leo nisi. Vestibulum vehicula, "
+                    "orci ac varius eleifend, ante erat efficitur tortor, "
+                    "quis tincidunt elit ex ut est.",
+                imagePath: "images/iphone.jpg",
+              ),
+              ProductWidget(
+                name: "GeForce RTX 4080 SUPER",
+                price: 1110,
+                description:
+                "Des jeux ultra-réalistes, ultra-fluides et ultra-immersifs, "
+                    "la carte graphique GeForce RTX 4080 SUPER met à votre "
+                    "disposition les technologies les plus avancées pour "
+                    "vous permettre de jouer dans les meilleures conditions, "
+                    "en très haute résolution ou en Réalité Virtuelle.",
+                imagePath: "images/cg.jpg",
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ intro-mobile/tuto1/lib/product_widget.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/material.dart';
+
+class ProductWidget extends StatelessWidget {
+  final String name;
+  final String description;
+  final int price;
+  final String imagePath;
+
+  const ProductWidget({
+    super.key,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imagePath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        width: 500,
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        name,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        "$price €",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    description,
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(width: 16),
+            Image.asset(imagePath, width: 150),
+          ],
+        ),
+      ),
+    );
+  }
+}
